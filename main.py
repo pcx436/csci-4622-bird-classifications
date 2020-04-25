da2ed50ce658a4a7756e06b8dd45f36062773c55
Jacob Malcy <jmalcy@protonmail.com>
2020-04-25 17:26:14 -0600

    Implement bounding box reading

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from PIL import Image
 
+# NOTE: All of the ids are 1 indexed in the 'images.txt' file, keep this in mind
 def build_id_list(filename):
     id_list = list()
 
@@ -12,11 +13,24 @@ def build_id_list(filename):
     return id_list
 
 
+# reads in the bounding boxes
+def bounding_boxes(filename):
+    boxes = list()
+    with open(filename, 'r') as boxes_file:
+        for line in boxes_file:
+            line_split = line.strip().split(' ')[1:]  # remove the index at the beginning of the line
+
+            converted_tuple = tuple([float(param) for param in line_split])  # convert coords to float, place in tuple
+            boxes.append(converted_tuple)
+
+    return boxes
+
+
 def main():
     images_directory = 'CUB_200_2011/images/'
 
-    # NOTE: All of the ids are 1 indexed in the 'images.txt' file, keep this in mind
     id_list = build_id_list('CUB_200_2011/images.txt')
+    boxes = bounding_boxes('CUB_200_2011/bounding_boxes.txt')
 
     # First image in the first category
     test_image_path = images_directory + id_list[0]
@@ -24,6 +38,7 @@ def main():
     test_image = Image.open(test_image_path)
 
     print('Test image dimensions:', test_image.size)
+    print('Test image bounding box:', boxes[0])
 
 
 if __name__ == '__main__':
