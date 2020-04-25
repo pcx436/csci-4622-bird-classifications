71d4c31721c5720ffbc83165ba8c1cedaf147569
Jacob Malcy <jmalcy@protonmail.com>
2020-04-25 17:05:23 -0600

    Loading test image

--- main.py
+++ main.py
@@ -2,8 +2,14 @@ from PIL import Image
 
 
 def main():
-    # placeholder main function
-    pass
+    images_directory = 'CUB_200_2011/images/'
+
+    # First image in the first category
+    test_image_path = images_directory + '001.Black_footed_Albatross/Black_Footed_Albatross_0001_796111.jpg'
+
+    test_image = Image.open(test_image_path)
+
+    print('Test image dimensions:', test_image.size)
 
 
 if __name__ == '__main__':
