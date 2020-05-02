4cc2e1185697d0ecf5c27921970543e9ccd91285
Jacob Malcy <jmalcy@protonmail.com>
2020-05-02 14:42:40 -0600

    WIP: check preprocessor works

--- model.py
+++ model.py
@@ -3,7 +3,9 @@ from preprocessor import preprocess
 
 def main():
     # TODO: start making that model!
-    pass
+    x_train, y_train, x_test, y_test, x_valid, y_valid = preprocess(seed=12345)
+
+    print(len(x_train))
 
 
 if __name__ == '__main__':
