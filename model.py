61c31830939e75a0607ab9f18fa6a85880e6f5e1
Jacob Malcy <jmalcy@protonmail.com>
2020-05-02 14:44:42 -0600

    WIP: Import keras and create build_model function

--- model.py
+++ model.py
@@ -1,11 +1,15 @@
 from preprocessor import preprocess
+from tensorflow import keras
+
+
+# TODO: start making that model!
+def build_model(x_train, y_train, x_test, y_test, x_valid, y_valid):
+    pass
 
 
 def main():
-    # TODO: start making that model!
+    # seed 12345 so not random data each time
     x_train, y_train, x_test, y_test, x_valid, y_valid = preprocess(seed=12345)
-
-    print(len(x_train))
 
 
 if __name__ == '__main__':
